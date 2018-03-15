c29c003c23ae43fed3e5a04cf64d748a60bcc0d3
Pau Cervera <pau.cervera@gmail.com>
2018-03-15 23:34:37 +0100

    Some more config, window manager and such

--- init.el
+++ init.el
@@ -32,7 +32,9 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (ido-vertical-mode exwm beacon use-package))))
+ '(package-selected-packages
+   (quote
+    (swiper ivy spaceline ido-vertical-mode exwm beacon use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
