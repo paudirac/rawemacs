f2e0aca58f251067db5697ebdd2fcd777ba91ea4
Pau Cervera <pau.cervera@gmail.com>
2018-06-28 06:49:07 +0200

    Emacs speaks statistics

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (swiper ivy spaceline ido-vertical-mode exwm beacon use-package))))
+    (ess evil projectile swiper ivy spaceline ido-vertical-mode exwm beacon use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
